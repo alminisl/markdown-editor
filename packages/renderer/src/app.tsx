9bdb5a9d49a188f83a7898a93cdd4aba825f036a
alminisl <almin.islamovic@gmail.com>
2022-01-22 23:37:19 +0100

    removing unused code

--- packages/renderer/src/app.tsx
+++ packages/renderer/src/app.tsx
@@ -1,13 +1,7 @@
 import React, { useState, useCallback } from 'react'
-import { Remarkable } from 'remarkable'
-import Editor from './editor'
-import Preview from './preview'
 import './app.css'
 import SimpleMdeReact from './SimpleMdeReact'
 import 'easymde/dist/easymde.min.css'
-
-const md = new Remarkable()
-md.inline.ruler.enable(['ins', 'mark'])
 
 const App: React.FC = () => {
   const [doc, setDoc] = useState<string>('# Hello, World!\n')
